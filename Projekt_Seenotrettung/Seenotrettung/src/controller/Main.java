975411c9060f52cb3eb383851f77ad517cd0ca4f
mirko killmann <Killmann.M.EIT12@rvwbk.de>
2023-03-07 14:20:14 +0100

    initial commit

---
+++ Projekt_Seenotrettung/Seenotrettung/src/controller/Main.java
@@ -0,0 +1,9 @@
+package controller;
+
+import frontend.FrontWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        FrontWindow.main(new String[]{});
+    }
+}
---
+++ Projekt_Seenotrettung/Seenotrettung/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Admin
+ *
+ */
+module Seenotrettung {
+	requires java.desktop;
+}
